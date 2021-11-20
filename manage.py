7a810830a6844327e5c2539cc41d5ed66e560718
Axrorbek1997 <axrorbekbebitovich97@gmail.com>
2021-11-20 16:37:26 +0500

    default port commit

--- manage.py
+++ manage.py
@@ -9,6 +9,12 @@ def main():
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'project.settings')
     try:
         from django.core.management import execute_from_command_line
+        from django.core.management.commands.runserver import Command
+
+        import django
+        django.setup()
+
+        Command.default_port = "8080"
     except ImportError as exc:
         raise ImportError(
             "Couldn't import Django. Are you sure it's installed and "
